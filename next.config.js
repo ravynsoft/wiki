80b7168050ca4209e831c7ec9ed780f3cfee0caf
jckgan <jack@nevuqe.com>
2023-06-15 22:31:54 +0100

    next.config: revert some changes
    reverted deletion of code which allows static images to be exported by next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const { remarkCodeHike } = require("@code-hike/mdx");
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js'
-})
- 
-module.exports = withNextra()
+  themeConfig: './theme.config.js',
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
+  // unstable_staticImage: true
+});
+module.exports = withNextra({
+  images: {
+    domains: ["pbs.twimg.com", "avatars.githubusercontent.com", "i.imgur.com"],
+    loader: 'akamai',
+    path: '/',
+  },
+});
