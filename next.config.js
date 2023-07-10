fa205c9a33f843c0348852f07576074b9fd5191e
jckgan <jack@nevuqe.com>
2023-07-10 19:26:37 +0100

    wiki: update deps & use image workaround

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_flexsearch: true,
-  unstable_staticImage: true,
-  // unstable_staticImage: true
+  flexsearch: true,
+  staticImage: true,
 });
 module.exports = withNextra({
   images: {
-    domains: ["pbs.twimg.com", "avatars.githubusercontent.com", "i.imgur.com"],
     loader: 'akamai',
     path: '/',
   },
