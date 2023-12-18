6707ea5f6c433c7b5a20edfb1896994db7d3922e
Jack Gannon <jack@nevuqe.com>
2023-12-18 18:28:17 +0000

    home: refactor quick links

--- public/assets/iconoir/index.js
+++ public/assets/iconoir/index.js
@@ -3,3 +3,6 @@ export { default as MergeIcon } from './merge.svg';
 export { default as WrenchIcon } from './wrench.svg';
 export { default as CDIcon } from './compact-disc.svg';
 export { default as SourceIcon } from './folder.svg';
+export { default as Settings } from './settings.svg';
+export { default as Environment } from './select-window.svg';
+export { default as Layout } from './layout.svg';
